0a183f80f47ca27c50fef118bd2d06cd81c50dd4
Nathaniel Roberts <nathanieljroberts11@gmail.com>
2020-03-05 22:18:45 -0800

    customized snake

--- app/server.py
+++ app/server.py
@@ -29,7 +29,7 @@ def start():
 	data = bottle.request.json
 	("START:", json.dumps(data))
 
-	response = {"color": "#00FF00", "headType": "regular", "tailType": "regular"}
+	response = {"color": "#4F1851", "headType": "evil", "tailType": "hook"}
 	return HTTPResponse(
 		status=200,
 		headers={"Content-Type": "application/json"},
